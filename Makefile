0862b69ead868fa83a390419bd6f7382a1e1feb2
Craig Robson <craig@zhatt.com>
2017-12-04 21:19:18 -0800

    Day 5.

--- Makefile
+++ Makefile
@@ -4,7 +4,8 @@ CXXFLAGS=-Wall -Werror -g
 all: day1.1 day1.2 \
 	day2.1 day2.2 \
 	day3.1 day3.2 \
-	day4.1 day4.2
+	day4.1 day4.2 \
+	day5.1 day5.2
 
 day1.1: day1.1.cpp
 day1.2: day1.2.cpp
@@ -14,4 +15,7 @@ day3.1: day3.1.cpp
 day3.2: day3.2.cpp
 day4.1: day4.1.cpp
 day4.2: day4.2.cpp
+day5.1: day5.1.cpp
+day5.2: day5.2.cpp
+
 
---
+++ day5.2.cpp
@@ -0,0 +1,44 @@
+/*
+--- Part Two ---
+
+Now, the jumps are even stranger: after each jump, if the offset was three or more, instead decrease it by 1. Otherwise, increase it by 1 as before.
+
+Using this rule with the above example, the process now takes 10 steps, and the offset values after finding the exit are left as 2 3 2 3 -1.
+
+How many steps does it now take to reach the exit?
+
+*/
+
+#include <iostream>
+#include <vector>
+
+int main() {
+
+	std::vector<int> data;
+
+	while ( std::cin ) {
+
+		int x;
+		std::cin >> x;
+		if ( std::cin ) data.push_back( x );
+	}
+
+	int current = 0;
+	int jumps = 0;
+
+	while ( current < (int) data.size() ) {
+		int oldcurrent = current;
+		current += data[current];
+
+		if ( data[oldcurrent] >= 3 ) {
+			data[oldcurrent]--;
+		} else {
+			data[oldcurrent]++;
+		}
+
+//		std::cout << "C:" << current << std::endl;
+		jumps++;
+	}
+
+	std::cout << jumps << std::endl;
+}
